0935bf22e842ebf998f6d1ecc0152982079d39b9
yusaku0515 <y.iwata0515@gmail.com>
2020-08-13 04:18:50 +0000

    [add]投稿関係のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations",
+    passwords: "users/passwords",
+  }
 
   resources :users, only: [:show, :edit, :update]
   get 'users/:id/leave' => 'users#leave', as: 'users_leave'
 
+  resources :posts
 
   get 'home/about' => 'homes#about'
   get 'home/top' => 'homes#top'
 
-     # エンドユーザー側のトップ画面
-   get '/' => 'users/homes#top', as: 'root'
+  # トップ画面
+  get '/' => 'homes#top', as: 'root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

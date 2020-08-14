cad2feb1ba6268e6ac9371bb37b87268e1b6dc8d
yusaku0515 <y.iwata0515@gmail.com>
2020-08-14 08:58:47 +0000

    [add]ユーザー情報の取得・編集・退会処理の機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,36 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
 
-	def show
-		@user = User.find(params[:id])
-	end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-	def edit
-	end
+  def update
+    @user = User.find(params[:id])
+    if params[:user][:member_status] == "false" # 退会用の記述
+      @user.user_status = false
+      @user.update(user_params)
+      reset_session # ログアウトする
+      redirect_to root_path
+      return # ここから下は実行されない
+    else
+      if @user.update(user_params) # 会員情報の編集
+        redirect_to user_path(@user.id), notice: "変更内容を更新しました"
+      else # エラーメッセージ表示用
+        render 'edit'
+        end
+    end
+  end
 
-	def update
-	end
+  def leave
+    @user = User.find(params[:id])
+  end
 
-	def leave
-	end
+  private
 
-	private
-
-	def user_params
-	end
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :user_status)
+  end
 end

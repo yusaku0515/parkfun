3d11c33a2a83e05317d70a3c8709de5d2867bd9b
yusaku0515 <y.iwata0515@gmail.com>
2020-08-22 14:09:41 +0000

    [add]お問い合わせ機能についてのコード追加

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,22 @@
 class ContactsController < ApplicationController
+  def new
+    @user = User.find_by(params[:id])
+  	@contact = Contact.new
+  end
 
-	def new
-	end
+  def create
+  	@contact = Contact.new(contact_params)
+  	if @contact.save
+  		ContactMailer.contact_mail(@contact).deliver
+  		redirect_to root_path, notice: "お問い合わせを受け付けました"
+  	else
+  		render :new, notice: "メールアドレスかお問い合わせ内容に空白があると送信できません"
+  	end
+  end
 
-	def create
-	end
+  private
 
+  def contact_params
+  	params.permit(:message, :user_id)
+  end
 end

b50e9cd8bfdef0b1febefb4ab967bb4b263354eb
yusaku0515 <y.iwata0515@gmail.com>
2020-08-28 21:10:16 +0000

    [add]admin側会員一覧ページ編集

---
+++ app/controllers/admins/users_controller.rb
@@ -0,0 +1,17 @@
+class Admins::UsersController < ApplicationController
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+	end
+end

d1aece2a4afb7525d57f4a745cf5c0e198f569a6
yusaku0515 <y.iwata0515@gmail.com>
2020-09-04 16:00:14 +0000

    [add]通知用ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :admins, controllers: {
-    sessions:      'admins/sessions',
-    passwords:     'admins/passwords',
-    registrations: 'admins/registrations'
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations',
   }
   devise_for :users, controllers: {
-    sessions:      'users/sessions',
+    sessions: 'users/sessions',
     registrations: 'users/registrations',
-    passwords:     'users/passwords',
+    passwords: 'users/passwords',
   }
 
   resources :users, only: [:show, :edit, :update]
@@ -20,19 +20,44 @@ Rails.application.routes.draw do
     # コメント
     resources :comments, only: [:create, :destroy]
   end
+
   # アバウト画面
   get 'home/about' => 'homes#about'
+
   # トップ画面
   get 'home/top' => 'homes#top'
+
+  # 通知機能
+  resources :notifications, only: :index
+  resources :notifications do
+    collection do
+      delete 'destroy_all'
+    end
+  end
 
   # お問い合わせ画面
   get 'contacts/new'
   post 'contacts/create'
 
   # 検索画面
-  get  'search' => 'searches#search',  as: 'search'
+  get  'search' => 'searches#search', as: 'search'
 
   # トップ画面
   get '/' => 'homes#top', as: 'root'
+
+  # admin側トップ画面
+  namespace :admins do
+    get 'homes/top'
+  end
+
+  # admin側/ユーザー関連
+  namespace :admins do
+    resources :users
+  end
+
+  # admin側/投稿関連
+  namespace :admins do
+    resources :posts
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
